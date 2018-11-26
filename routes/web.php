57b74fbff96b95d08369f53fcbc2a64be2acacc9
Bas Klein <bas-klein@live.nl>
2018-11-26 13:33:37 +0100

    v0.2

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
+
+Route::resource('posts', 'PostsController');
 // Route::get('/users/{id}', function($id){
 // 	return 'This is user'.$id;
 // });
